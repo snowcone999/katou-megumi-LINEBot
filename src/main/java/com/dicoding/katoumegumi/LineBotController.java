9860ea77e4e6758ac3741be2ba9008768ed680da
Akmal Maulana Azzam <akmalazzam2240@gmail.com>
2017-07-04 06:38:51 +0700

    fixing youtube error 1

--- src/main/java/com/dicoding/katoumegumi/LineBotController.java
+++ src/main/java/com/dicoding/katoumegumi/LineBotController.java
@@ -553,7 +553,7 @@ public class LineBotController
                 listUrl.add(d.url);
             }
 
-            urlDownload = String.valueOf(listUrl.get(3));
+            urlDownload = String.valueOf(listUrl.get(2));
         } catch (Exception e) {
             e.printStackTrace();
         }
